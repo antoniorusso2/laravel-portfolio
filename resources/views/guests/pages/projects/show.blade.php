fcb6f530c0798a22dea15f239460c729bb181fad
antoniorusso2 <russoantonio303@outlook.it>
2025-03-27 19:28:30 +0100

    add base layout for detail page

--- resources/views/guests/pages/projects/show.blade.php
+++ resources/views/guests/pages/projects/show.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
+    <div class="container mt-5">
+        <div class="row row-gap-3">
             <div class="col-12">
                 <h1>{{ $project->name }}</h1>
+            </div>
+            <div class="col-12 col-md-6">
+                <img src="https://placehold.co/600x400" alt="screenshot project" class="card-img-top">
+            </div>
+            <div class="col-12 col-md-6">
                 <p>{{ $project->description }}</p>
-                <p>Cliente: {{ $project->customer }}</p>
             </div>
         </div>
     </div>
