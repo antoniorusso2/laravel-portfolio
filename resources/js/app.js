169f0dd881fcec1d5a722f770623e6fb73e40065
antoniorusso2 <russoantonio303@outlook.it>
2025-06-29 08:46:05 +0200

    remove unnecessary files and readded core files as layout components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
-import './bootstrap';
-import '~resources/scss/app.scss';
-import '~icons/bootstrap-icons.scss';
-import * as bootstrap from 'bootstrap';
-import.meta.glob([
-    '../img/**'
-])
+import "./bootstrap";
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.start();
